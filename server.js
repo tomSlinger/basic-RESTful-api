69870fb823aa41dfb4f730e42b1f1583f4f6716b
Tom Slinger <t_o_m_ouk@hotmail.com>
2016-12-16 16:59:53 +0000

    configure app to use bodyparser

--- server.js
+++ server.js
@@ -5,3 +5,8 @@
 var express = require('express'); //CALL EXPRESS
 var app = express(); //DEFINE OUR APP USING EXPRESS
 var bodyParser = require('body-parser');
+
+//CONFIGURE APP TO USE BODYPARSER()
+//THIS WILL LET US GET THE DATA FROM A POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
