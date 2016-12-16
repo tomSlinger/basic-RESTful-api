f3724e05a35b208f16c8e3e8e57aa981a7482a9e
Tom Slinger <t_o_m_ouk@hotmail.com>
2016-12-16 16:58:32 +0000

    call packages

---
+++ server.js
@@ -0,0 +1,7 @@
+//BASE SETUP
+// ==============================================================================================================
+
+//CALL THE PACKAGES WE NEED
+var express = require('express'); //CALL EXPRESS
+var app = express(); //DEFINE OUR APP USING EXPRESS
+var bodyParser = require('body-parser');
