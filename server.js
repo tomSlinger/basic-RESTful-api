8aae2e31edd89ac614affb0f8854c4a984ea2a46
Tom Slinger <t_o_m_ouk@hotmail.com>
2016-12-16 17:28:11 +0000

    changed api test message

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var router = express.Router(); //GET AN INSTANCE OF THE EXPRESS ROUTER
 
 //TEST ROUTE TO MAKE SURE EVERYTHING IS WORKING (ACCESSED AT GET HTTP://LOCALHOST:8080/API)
 router.get('/', function(req, res){
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.json({ message: 'Hello! Welcome to our API!' });
 });
 
 //MORE ROUTES FOR OUR API WILL HAPPEN HERE
